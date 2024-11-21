af9c21c95f5b1b05d442452f869169bea90d30a0
Alfred Morgan <alfredhmcode@gmail.com>
2024-11-21 16:45:38 -0600

    x

--- root/server/middleware/auth.js
+++ root/server/middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function auth(req, res, next) {
     if (!token) return res.status(401).send('Access denied. No token provided.');
 
     try {
-        const decoded = jwt.verify(token, process.env.jetPrivateKey); // Verify token
+        const decoded = jwt.verify(token, process.env.jwtPrivateKey); // Verify token
         req.user = decoded
         next()
     }
