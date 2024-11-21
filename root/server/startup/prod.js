f15b796dc272df0c102ea54f4462b752a8c4fd7b
Alfred Morgan <alfredhmcode@gmail.com>
2024-11-21 13:23:41 -0600

    Fix node_modules nesting issue

--- root/server/startup/prod.js
+++ root/server/startup/prod.js
@@ -1,7 +1,18 @@
 const helmet = require('helmet');   
 const compression = require('compression');
+const express = require("express");
+const path = require("path");
 
 module.exports = function(app) {
+    // Serve React static files in production
+    if (process.env.NODE_ENV === "production") {
+        const clientBuildPath = path.join(__dirname, "../client/build");
+        app.use(express.static(clientBuildPath));
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(clientBuildPath, "index.html"));
+        });
+    }
     app.use(helmet());
     app.use(compression());
+
 }
