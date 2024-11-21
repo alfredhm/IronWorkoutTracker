2c5c33e1a8252a82bc97701736862e733b5eb7f0
Alfred Morgan <alfredhmcode@gmail.com>
2024-11-21 13:38:02 -0600

    x

--- root/server/startup/prod.js
+++ root/server/startup/prod.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = function(app) {
     // Serve React static files in production
     if (process.env.NODE_ENV === "production") {
-        const clientBuildPath = path.join(__dirname, "../client/build");
+        const clientBuildPath = path.join(__dirname, "../../client/build");
         app.use(express.static(clientBuildPath));
         app.get("*", (req, res) => {
             res.sendFile(path.join(clientBuildPath, "index.html"));
