18a2de4da607156ec93ccbfe8e247744ac4d8e76
Alfred Morgan <alfredhmcode@gmail.com>
2024-11-21 15:32:53 -0600

    x

--- root/server/startup/db.js
+++ root/server/startup/db.js
@@ -1,7 +1,13 @@
-const winston = require('winston')
-const mongoose = require('mongoose')
+const winston = require('winston');
+const mongoose = require('mongoose');
 
-module.exports = function() {
-    mongoose.connect('mongodb://localhost/tracker')
-      .then(() => winston.info("Connected to MongoDB"))
-}
+module.exports = function () {
+    const db = process.env.MONGO_URI || 'mongodb://localhost/tracker'; // Use environment variable or fallback to localhost
+    mongoose
+        .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => winston.info(`Connected to MongoDB at ${db}`))
+        .catch((err) => {
+            winston.error(`Failed to connect to MongoDB: ${err.message}`);
+            process.exit(1); // Exit if the database connection fails
+        });
+};
